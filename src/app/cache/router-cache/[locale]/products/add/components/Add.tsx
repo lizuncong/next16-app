9380edfdf0e9be8f5635088a0319ae2b29d27a31
lizuncong <2900170896@qq.com>
2026-01-04 00:07:04 +0800

    feat: add

--- src/app/cache/router-cache/[locale]/products/add/components/Add.tsx
+++ src/app/cache/router-cache/[locale]/products/add/components/Add.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { useState } from "react";
 import { addProduct } from "../action";
 
 export const AddBtn = () => {
+  const [list, setList] = useState([]);
   return (
-    <button className="text-blue-500" onClick={() => addProduct()}>
-      点击添加商品
+    <button
+      className="text-blue-500"
+      onClick={async () => {
+        const res = await addProduct();
+        console.log("res...1", res);
+        setList(res.data);
+      }}
+    >
+      点击添加商品{list.length}
     </button>
   );
 };
